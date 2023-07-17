1af123511f4357f964aa105a17b54a324f8a1c25
Michael Salvatore <msalvatore22@gmail.com>
2023-07-17 15:50:27 -0400

    Implement server side cookie based auth, update of bio through server

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,8 +7,23 @@
 
     async function signInWithGoogle() {
         const provider = new GoogleAuthProvider();
-        const user = await signInWithPopup(auth, provider);
-        console.log(user)
+        const credential = await signInWithPopup(auth, provider);
+
+        const idToken = await credential.user.getIdToken();
+
+        const res = await fetch("/api/signin", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+        },
+        body: JSON.stringify({ idToken }),
+        });
+    }
+
+    async function signOutSSR() {
+        const res = await fetch("/api/signin", { method: "DELETE" });
+        await signOut(auth);
     }
 
 </script>
@@ -18,7 +33,7 @@
 {#if $user}
     <h2 class='card-title'>Welcome, {$user.displayName}</h2>
     <p class="text-center text-success">You are logged in</p>
-    <button class="btn btn-danger" on:click={() => signOut(auth)}>Sign Out</button>
+    <button class="btn btn-danger" on:click={() => signOutSSR()}>Sign Out</button>
 {:else}
     <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
